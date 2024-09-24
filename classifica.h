a6445731f2ce016feb7ddd38574c180e1ceb29b3
Elisei <mailatelisei27@gmail.com>
2024-09-24 18:30:27 +0200

    Added the fist Functions signature in header

--- classifica.h
+++ classifica.h
@@ -5,4 +5,15 @@
 #ifndef CLASSIFICA_MAILAT_CLASSIFICA_H
 #define CLASSIFICA_MAILAT_CLASSIFICA_H
 
+#include <string>
+
+typedef struct {
+    std::string nome;
+    int punteggio;
+} Player;
+
+void mostraClassifica(Player player, Player classifica[]);
+
+void inserisciGiocatore(Player player, Player classifica[]);
+
 #endif //CLASSIFICA_MAILAT_CLASSIFICA_H
