64aafc3f33329fdbb4fbce4ecdd95323eda09bf3
Elisei <mailatelisei27@gmail.com>
2024-09-24 21:31:40 +0200

    Implemented main()

--- main.cpp
+++ main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include <string>
 #include "classifica.h"
+
+void Menu(){
+    std::cout << std::endl;
+    std::cout << " _______________________________________________________\n";
+    std::cout << "|           CLASSIFICA - Elisei Mailat 4AI              |\n";
+    std::cout << "|-------------------------------------------------------|\n";
+    std::cout << "| [1] Visualizza classifica     [2] Inserisci giocatore |\n";
+    std::cout << "| [0] Exit.                                             |\n";
+    std::cout << "|_______________________________________________________|\n";
+    std::cout << " Scelta -> ";
+}
+
 
 int main() {
 
+    Player classifica[DIM_CLASSIFICA];
+    initClassifica(classifica);
 
+    while(true) {
 
-    return 0;
+        Player giocatore;
+
+        int userInput;
+        Menu();
+        std::cin >> userInput;
+
+        //Se l'input non e' valido (es. l'utente inserisce una lettera)
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "----------------------------------------------\n";
+            std::cout << "Errore: Input non valido, inserisci un numero!\n";
+            continue;
+        }
+
+        switch(userInput){
+            case 1:
+                mostraClassifica(classifica);
+                break;
+            case 2:
+
+                std::cout << " _______________________________________________________\n";
+                std::cout << "|                  INSERISCI GIOCATORE                  |\n";
+                std::cout << " -------------------------------------------------------\n";
+
+                std::cout << "Inserisci il nome -> ";
+                std::cin >> giocatore.nome;
+
+                std::cout << "("<<giocatore.nome<<") Inserisci il punteggio -> ";
+                std::cin >> giocatore.punteggio;
+
+                inserisciGiocatore(classifica, giocatore.nome, giocatore.punteggio);
+
+                break;
+            case 0:
+                return 0;
+        }
+    }
 }
