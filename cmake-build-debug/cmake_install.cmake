a44e20e457f9e818a1b657e3997026752ccd377b
Elisei <mailatelisei27@gmail.com>
2024-09-24 18:32:17 +0200

    Updated all the files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/eliseimailat/CLionProjects/Classifica_Mailat
+# Install script for directory: /Users/eliseimailat/Classifica_Mailat
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/eliseimailat/CLionProjects/Classifica_Mailat/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/eliseimailat/Classifica_Mailat/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "classifica.h"
 
 int main() {
+
 
 
     return 0;
