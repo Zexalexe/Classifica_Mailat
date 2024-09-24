80d4650f27e702b0c801d61d05b43b070d642dc2
Elisei <mailatelisei27@gmail.com>
2024-09-24 18:44:02 +0200

    Updated header file

--- classifica.h
+++ classifica.h
@@ -7,13 +7,17 @@
 
 #include <string>
 
+const int DIM_CLASSIFICA = 10;
+
 typedef struct {
     std::string nome;
     int punteggio;
 } Player;
 
-void mostraClassifica(Player player, Player classifica[]);
+void initClassifica(Player classifica[]);
 
-void inserisciGiocatore(Player player, Player classifica[]);
+void mostraClassifica(Player classifica[]);
+
+void inserisciGiocatore(Player classifica[]);
 
 #endif //CLASSIFICA_MAILAT_CLASSIFICA_H
